2037b01a24a1ac5f8b44a6b14832b779de202da6
Pedro09Mera <merap697@gmail.com>
2023-04-27 16:47:39 -0500

    prcatica laravel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,1 @@
-<?php
-use Illuminate\Support\Facades\DB;
-?>
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Versión de SQL Server</title>
-</head>
-<body>
-    <?php
-        $version = DB::select('select @@version as version')[0]->version;
-        echo "La versión de SQL Server es: ".$version;
-    ?>
-</body>
-</html>
+
